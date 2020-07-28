aed9ebac5228532d5496add11a2eead4bc63eb1f
Nikolay Koldunov <koldunovn@gmail.com>
2020-07-28 13:34:14 +0200

    formatting

--- ftest/ftest.py
+++ ftest/ftest.py
@@ -1,21 +1,32 @@
+import argparse
+import glob
+import json
+import os
 import time
+from collections import OrderedDict
+from datetime import date
+
+import joblib
+import pkg_resources
+import yaml
 from fabric import Connection
 from invoke import Responder
-from collections import OrderedDict
-import os
-from datetime import date
-import joblib
-import yaml
-import pkg_resources
-import argparse
-import json
-import glob
 
 finish_codes = [
-    "BOOT_FAIL", "CANCELLED", "COMPLETED", "DEADLINE", "FAILED", "NODE_FAIL",
-    "OUT_OF_MEMORY", "PREEMPTED", "REVOKED", "SPECIAL_EXIT", "STOPPED",
-    "TIMEOUT"
+    "BOOT_FAIL",
+    "CANCELLED",
+    "COMPLETED",
+    "DEADLINE",
+    "FAILED",
+    "NODE_FAIL",
+    "OUT_OF_MEMORY",
+    "PREEMPTED",
+    "REVOKED",
+    "SPECIAL_EXIT",
+    "STOPPED",
+    "TIMEOUT",
 ]
+
 
 class EnvVarLoader(yaml.SafeLoader):
     pass
@@ -28,138 +39,144 @@ def read_yml(yml_path):
 
 
 def connect(machine):
-    if 'ssh_key' in machine:
+    if "ssh_key" in machine:
         if "ssh_pass" in machine:
-            ssh_pass = machine['ssh_pass']
+            ssh_pass = machine["ssh_pass"]
         else:
             ssh_pass = None
-        c = Connection(machine['adress'],
-                    user=machine['user'],
-                    connect_kwargs={"key_filename": machine['ssh_key'], "passphrase": ssh_pass})
+        c = Connection(
+            machine["adress"],
+            user=machine["user"],
+            connect_kwargs={"key_filename": machine["ssh_key"], "passphrase": ssh_pass},
+        )
     elif "password" in machine:
-        c = Connection(machine['adress'],
-                    user=machine['user'],
-                    connect_kwargs={"password": machine['password']})
-        
+        c = Connection(
+            machine["adress"],
+            user=machine["user"],
+            connect_kwargs={"password": machine["password"]},
+        )
+
     return c
 
 
 def record_result(test_results, action, result):
     test_results[action] = OrderedDict()
     if result.ok:
-        test_results[action]['status'] = 'OK'
-        test_results["latest"] = 'OK'
+        test_results[action]["status"] = "OK"
+        test_results["latest"] = "OK"
     else:
-        test_results[action]['status'] = 'FAIL'
-        test_results["latest"] = 'FAIL'
+        test_results[action]["status"] = "FAIL"
+        test_results["latest"] = "FAIL"
 
-    test_results[action]['stdout'] = result.stdout
-    test_results[action]['stderr'] = result.stderr
+    test_results[action]["stdout"] = result.stdout
+    test_results[action]["stderr"] = result.stderr
 
     return test_results
 
 
 def record_no_run(test_results, action):
     test_results[action] = OrderedDict()
-    test_results[action]['status'] = 'NOT RUN'
-    test_results[action]['stdout'] = 'NOT RUN'
-    test_results[action]['stderr'] = 'NOT RUN'
-    test_results["latest"] = 'FAIL'
+    test_results[action]["status"] = "NOT RUN"
+    test_results[action]["stdout"] = "NOT RUN"
+    test_results[action]["stderr"] = "NOT RUN"
+    test_results["latest"] = "FAIL"
     return test_results
 
 
-def clone(c, test_results, experiment, login='', passs=''):
+def clone(c, test_results, experiment, login="", passs=""):
 
-    if test_results['latest'] != 'OK':
-        test_results = record_no_run(test_results, 'clone')
+    if test_results["latest"] != "OK":
+        test_results = record_no_run(test_results, "clone")
         return test_results
 
-    repo = experiment['repo']
-    branch = experiment['branch']
+    repo = experiment["repo"]
+    branch = experiment["branch"]
 
-    result = c.run('mkdir test_today', warn=True, echo=True)
+    result = c.run("mkdir test_today", warn=True, echo=True)
     if result.failed:
-        c.run('rm -rf test_today; mkdir test_today', warn=True, echo=True)
+        c.run("rm -rf test_today; mkdir test_today", warn=True, echo=True)
 
-    responder_user = Responder(
-        pattern=r"Username for",
-        response=f"{login}\n",
+    responder_user = Responder(pattern=r"Username for", response=f"{login}\n",)
+    responder_pass = Responder(pattern=r"Password for", response=f"{passs}\n",)
+
+    result = c.run(
+        f"module load git; cd test_today;\
+                git clone -b {branch} {repo}",
+        echo=True,
+        pty=True,
+        watchers=[responder_user, responder_pass],
     )
-    responder_pass = Responder(
-        pattern=r"Password for",
-        response=f"{passs}\n",
-    )
 
-    result = c.run(f'module load git; cd test_today;\
-                git clone -b {branch} {repo}',
-                   echo=True,
-                   pty=True,
-                   watchers=[responder_user, responder_pass])
-
-    test_results = record_result(test_results, 'clone', result)
-    test_results['latest'] = 'OK'
+    test_results = record_result(test_results, "clone", result)
+    test_results["latest"] = "OK"
 
     return test_results
 
 
 def build(c, test_results):
 
-    if test_results['latest'] != 'OK':
-        test_results = record_no_run(test_results, 'build')
+    if test_results["latest"] != "OK":
+        test_results = record_no_run(test_results, "build")
         return test_results
 
-    result = c.run('cd test_today/fesom2;\
-                    bash -l configure.sh',
-                   warn=True,
-                   echo=True)
-    test_results = record_result(test_results, 'build', result)
-    test_results['latest'] = 'OK'
+    result = c.run(
+        "cd test_today/fesom2;\
+                    bash -l configure.sh",
+        warn=True,
+        echo=True,
+    )
+    test_results = record_result(test_results, "build", result)
+    test_results["latest"] = "OK"
 
     return test_results
 
 
 def mkrun(c, test_results, machine, experiment):
 
-    if test_results['latest'] != 'OK':
-        test_results = record_no_run(test_results, 'mkrun')
+    if test_results["latest"] != "OK":
+        test_results = record_no_run(test_results, "mkrun")
         return test_results
 
-    experiment_name = experiment['experiment_name']
-    parent_experiment = experiment['parent_experiment']
+    experiment_name = experiment["experiment_name"]
+    parent_experiment = experiment["parent_experiment"]
 
     mkrun_line = f"mkrun {experiment_name} {parent_experiment}"
 
-    if 'account' in machine:
+    if "account" in machine:
         mkrun_line = mkrun_line + " " + f"-a {machine['account']}"
 
     python_env = machine["python_env"]
-    result = c.run(f' {python_env} ;\
+    result = c.run(
+        f" {python_env} ;\
                 cd test_today/fesom2/;\
-                {mkrun_line}',
-                   warn=True,
-                   echo=True)
-    test_results = record_result(test_results, 'mkrun', result)
-    test_results['latest'] = 'OK'
+                {mkrun_line}",
+        warn=True,
+        echo=True,
+    )
+    test_results = record_result(test_results, "mkrun", result)
+    test_results["latest"] = "OK"
 
     return test_results
 
 
 def submit(c, test_results, machine, experiment):
 
-    if test_results['latest'] != 'OK':
-        test_results = record_no_run(test_results, 'submit')
+    if test_results["latest"] != "OK":
+        test_results = record_no_run(test_results, "submit")
         return test_results
 
-    experiment_name = experiment['experiment_name']
-    machine_name = machine['name']
+    experiment_name = experiment["experiment_name"]
+    machine_name = machine["name"]
     work_dir = f"test_today/fesom2/work_{experiment_name}"
-    result = c.run(f'cd {work_dir};\
-                sbatch job_{machine_name}_new',
-                   warn=True,
-                   echo=True)
+    result = c.run(
+        f"cd {work_dir};\
+                sbatch job_{machine_name}_new",
+        warn=True,
+        echo=True,
+    )
 
-    test_results = record_result(test_results, 'job_submit', result)
-    test_results['latest'] = 'OK'
+    test_results = record_result(test_results, "job_submit", result)
+    test_results["latest"] = "OK"
     return test_results
 
 
@@ -170,31 +187,33 @@ def parce_job_submit(stdout):
         jobid = "FAIL"
     return jobid
 
+
 def get_status_juwels(c, jobid):
     if jobid == "FAIL":
         status = "NOT RUN"
         return status
 
-    result = c.run(f'sacct -j {jobid}', warn=True, hide=True)
-    status = 'UNDEFINED'
+    result = c.run(f"sacct -j {jobid}", warn=True, hide=True)
+    status = "UNDEFINED"
     if result.ok:
         for line in result.stdout.splitlines():
-            if 'fes_juwel' in line:
+            if "fes_juwel" in line:
                 print(line.split()[-2])
                 status = line.split()[-2]
 
     return status.strip()
+
 
 def get_status_mistral(c, jobid):
     if jobid == "FAIL":
         status = "NOT RUN"
         return status
 
-    result = c.run(f'sacct -j {jobid}', warn=True, hide=True)
-    status = 'UNDEFINED'
+    result = c.run(f"sacct -j {jobid}", warn=True, hide=True)
+    status = "UNDEFINED"
     if result.ok:
         for line in result.stdout.splitlines():
-            if 'fesom.x' in line:
+            if "fesom.x" in line:
                 print(line.split()[-2])
                 status = line.split()[-2]
 
@@ -206,7 +225,7 @@ def get_status_ollie(c, jobid):
         status = "NOT RUN"
         return status
 
-    result = c.run('sudo get_my_jobs.sh -d1', warn=True, hide=True)
+    result = c.run("sudo get_my_jobs.sh -d1", warn=True, hide=True)
     status = "UNDEFINED"
     if result.ok:
         lines = []
@@ -214,38 +233,40 @@ def get_status_ollie(c, jobid):
             if jobid in line:
                 lines.append(line)
         for line in lines:
-            if 'fesom.x' in line:
+            if "fesom.x" in line:
                 print(line.split()[-1])
                 status = line.split()[-1]
-                
+
     return status.strip()
 
 
 def query_status(c, jobid, machine):
-    if machine['name'] == 'mistral':
+    if machine["name"] == "mistral":
         status = get_status_mistral(c, jobid)
-    elif machine['name'] == 'ollie':
+    elif machine["name"] == "ollie":
         status = get_status_ollie(c, jobid)
-    elif machine['name'] == 'juwels':
+    elif machine["name"] == "juwels":
         status = get_status_juwels(c, jobid)
     else:
-        print(f'Unsupported machine {machine} for query the job status.')
+        print(f"Unsupported machine {machine} for query the job status.")
     return status
 
 
 def exit_status(c, test_results, machine, jobid, sleep=10, attempts=10):
 
-    if test_results['latest'] != 'OK':
-        test_results = record_no_run(test_results, 'exit_status')
+    if test_results["latest"] != "OK":
+        test_results = record_no_run(test_results, "exit_status")
         return test_results
-    
+
     if "exit_status_sleep" in machine:
-        sleep = machine['exit_status_sleep']
+        sleep = machine["exit_status_sleep"]
 
-    if 'exit_status_attempts' in machine:
-        attempts = machine['exit_status_attempts']
+    if "exit_status_attempts" in machine:
+        attempts = machine["exit_status_attempts"]
 
-    print(f"We will make {attempts} attempts with {sleep} second intervals to get the status.")
+    print(
+        f"We will make {attempts} attempts with {sleep} second intervals to get the status."
+    )
     status = "UNDEFINED"
     attempt = 0
 
@@ -256,14 +277,14 @@ def exit_status(c, test_results, machine, jobid, sleep=10, attempts=10):
         attempt += 1
 
     test_results["exit_status"] = {}
-    test_results["exit_status"]['status'] = status
-    test_results["exit_status"]['stdout'] = " "
-    test_results["exit_status"]['stderr'] = " "
+    test_results["exit_status"]["status"] = status
+    test_results["exit_status"]["stdout"] = " "
+    test_results["exit_status"]["stderr"] = " "
 
     if status == "COMPLETED":
-        test_results['latest'] = 'OK'
+        test_results["latest"] = "OK"
     else:
-        test_results['latest'] = 'FAIL'
+        test_results["latest"] = "FAIL"
 
     print(status)
     return test_results
@@ -271,30 +292,32 @@ def exit_status(c, test_results, machine, jobid, sleep=10, attempts=10):
 
 def check(c, test_results, machine, experiment):
 
-    if test_results['latest'] != 'OK':
-        test_results = record_no_run(test_results, 'fcheck')
+    if test_results["latest"] != "OK":
+        test_results = record_no_run(test_results, "fcheck")
         return test_results
 
-    experiment_name = experiment['experiment_name']
+    experiment_name = experiment["experiment_name"]
 
     python_env = machine["python_env"]
     tolerance = float(experiment["tolerance"])
     work_dir = f"test_today/fesom2/work_{experiment_name}"
-    result = c.run(f'{python_env};\
+    result = c.run(
+        f"{python_env};\
                 cd {work_dir};\
-                fcheck . -a {tolerance}',
-                   warn=True,
-                   echo=True)
+                fcheck . -a {tolerance}",
+        warn=True,
+        echo=True,
+    )
 
-    test_results = record_result(test_results, 'fcheck', result)
-    test_results['latest'] = 'OK'
+    test_results = record_result(test_results, "fcheck", result)
+    test_results["latest"] = "OK"
 
     return test_results
 
+
 def list_experiments():
-    settings_path = pkg_resources.resource_filename(__name__,
-                                                f'settings/')
-    experiment_paths = glob.glob(f'{settings_path}/*')
+    settings_path = pkg_resources.resource_filename(__name__, f"settings/")
+    experiment_paths = glob.glob(f"{settings_path}/*")
     experiment_paths.sort()
 
     for experiment in experiment_paths:
@@ -302,29 +325,18 @@ def list_experiments():
 
 
 def ftest():
-    parser = argparse.ArgumentParser(prog="ftest",
-                                     description="run FESOM2 tests.")
-    parser.add_argument("ename", help="Name of the experiment", default='ollie_sanity', nargs='*')
+    parser = argparse.ArgumentParser(prog="ftest", description="run FESOM2 tests.")
+    parser.add_argument(
+        "ename", help="Name of the experiment", default="ollie_sanity", nargs="*"
+    )
 
     parser.add_argument(
-        "--opath",
-        "-o",
-        type=str,
-        default='./odata/',
-        help="Output directory",
+        "--opath", "-o", type=str, default="./odata/", help="Output directory",
     )
     parser.add_argument(
-        "--branch",
-        "-b",
-        type=str,
-        default=None,
-        help="test branch",
+        "--branch", "-b", type=str, default=None, help="test branch",
     )
-    parser.add_argument(
-        "--list",
-        "-l",
-        action='store_true'
-    )
+    parser.add_argument("--list", "-l", action="store_true")
 
     args = parser.parse_args()
 
@@ -334,54 +346,57 @@ def ftest():
 
     experiment_name = args.ename[0]
 
-    experiment_settings_path = pkg_resources.resource_filename(__name__,
-                                                    f'settings/{experiment_name}/settings.yml')
-    homedir = os.path.expanduser('~')
-    machine_settings_path = f'{homedir}/.ftest/machines.yml'
-    repo_settings_path = f'{homedir}/.ftest/repos.yml'
+    experiment_settings_path = pkg_resources.resource_filename(
+        __name__, f"settings/{experiment_name}/settings.yml"
+    )
+    homedir = os.path.expanduser("~")
+    machine_settings_path = f"{homedir}/.ftest/machines.yml"
+    repo_settings_path = f"{homedir}/.ftest/repos.yml"
 
     machine_settings = read_yml(machine_settings_path)
     repo_settings = read_yml(repo_settings_path)
     experiment = read_yml(experiment_settings_path)
-    machine = machine_settings[experiment['machine']]
+    machine = machine_settings[experiment["machine"]]
 
     if args.branch is not None:
-        experiment['branch'] = args.branch
+        experiment["branch"] = args.branch
 
     test_results = OrderedDict()
-    test_results['latest'] = 'OK'
+    test_results["latest"] = "OK"
     today = date.today().strftime("%Y-%m-%d")
-    test_results['date'] = today
+    test_results["date"] = today
 
     c = connect(machine)
-    test_results = clone(c, test_results, experiment,
-                         login = repo_settings['login'],
-                          passs= repo_settings['pass'])
+    test_results = clone(
+        c,
+        test_results,
+        experiment,
+        login=repo_settings["login"],
+        passs=repo_settings["pass"],
+    )
     test_results = build(c, test_results)
     test_results = mkrun(c, test_results, machine, experiment)
     test_results = submit(c, test_results, machine, experiment)
 
-    jobid = parce_job_submit(test_results['job_submit']['stdout'])
-    
-    test_results = exit_status(c,
-                            test_results,
-                            machine,
-                            jobid,
-                            sleep=10,
-                            attempts=10)
+    jobid = parce_job_submit(test_results["job_submit"]["stdout"])
+
+    test_results = exit_status(c, test_results, machine, jobid, sleep=10, attempts=10)
     test_results = check(c, test_results, machine, experiment)
 
-    del test_results['latest']
+    del test_results["latest"]
     c.close()
 
-    ofolder = os.path.join(args.opath , experiment["experiment_name"])
+    ofolder = os.path.join(args.opath, experiment["experiment_name"])
 
     if not os.path.exists(ofolder):
         os.makedirs(ofolder)
 
-    ofile_name = os.path.join(ofolder, experiment["experiment_name"] + "_" + today+".json")
-    with open(ofile_name, 'w') as outfile:
+    ofile_name = os.path.join(
+        ofolder, experiment["experiment_name"] + "_" + today + ".json"
+    )
+    with open(ofile_name, "w") as outfile:
         json.dump(test_results, outfile)
+
 
 if __name__ == "__main__":
     # args = parser.parse_args()
--- ftest/sanityhtml.py
+++ ftest/sanityhtml.py
@@ -1,34 +1,30 @@
-import glob
-import os
-import json
-from jinja2 import Environment, FileSystemLoader
-import pkg_resources
 import argparse
+import glob
+import json
+import os
 import shutil
 
+import pkg_resources
+from jinja2 import Environment, FileSystemLoader
 
-templates_path = pkg_resources.resource_filename(__name__,
-                                                    f'templates')
+templates_path = pkg_resources.resource_filename(__name__, f"templates")
 file_loader = FileSystemLoader(templates_path)
 env = Environment(loader=file_loader)
 
 
 def sanityhtml():
-    parser = argparse.ArgumentParser(prog="sanityhtml",
-                                     description="Create html representation for FESOM2 tests.")
+    parser = argparse.ArgumentParser(
+        prog="sanityhtml", description="Create html representation for FESOM2 tests."
+    )
     parser.add_argument(
-        "--ipath",
-        "-i",
-        type=str,
-        default='./odata/',
-        help="Output directory",
+        "--ipath", "-i", type=str, default="./odata/", help="Output directory",
     )
     args = parser.parse_args()
 
     if not os.path.exists(args.ipath):
-        raise FileNotFoundError(f'There is no {args.ipath} directory.')
+        raise FileNotFoundError(f"There is no {args.ipath} directory.")
 
-    experiment_paths = glob.glob(f'{args.ipath}/*')
+    experiment_paths = glob.glob(f"{args.ipath}/*")
     experiment_paths.sort()
 
     experiment_names = []
@@ -36,8 +32,8 @@ def sanityhtml():
         experiment_names.append(os.path.basename(experiment))
 
     cn = {}
-    cn['title'] = 'FESOM2 sanity checks'
-    cn['experiments'] = {}
+    cn["title"] = "FESOM2 sanity checks"
+    cn["experiments"] = {}
 
     for experiment_name, experiment_path in zip(experiment_names, experiment_paths):
         experiment_files = glob.glob(f"{experiment_path}/*")
@@ -46,30 +42,30 @@ def sanityhtml():
         with open(experiment_last_file, "r") as read_file:
             test_results = json.load(read_file)
 
-        cn['experiments'][experiment_name] = test_results
+        cn["experiments"][experiment_name] = test_results
 
-    ofile = open('index.html', 'w')
-    template = env.get_template('index.html')
+    ofile = open("index.html", "w")
+    template = env.get_template("index.html")
     output = template.render(cn)
     ofile.write(output)
     ofile.close()
 
     for experiment_name in experiment_names:
-        ofolder = f'./ohtml/{experiment_name}/'
+        ofolder = f"./ohtml/{experiment_name}/"
         if not os.path.exists(ofolder):
             os.makedirs(ofolder)
-        date = cn['experiments'][experiment_name]['date']
-        ofilename = date+".html"
+        date = cn["experiments"][experiment_name]["date"]
+        ofilename = date + ".html"
         opath = os.path.join(ofolder, ofilename)
-        ofile = open(opath, 'w')
-        template = env.get_template('experiment.html')
-        output = template.render(cn['experiments'][experiment_name])
+        ofile = open(opath, "w")
+        template = env.get_template("experiment.html")
+        output = template.render(cn["experiments"][experiment_name])
         ofile.write(output)
         ofile.close()
-    
+
     for experiment_name in experiment_names:
-        ofolder = f'./ohtml/{experiment_name}/'
-        html_files = glob.glob(f'{ofolder}/*.html')
+        ofolder = f"./ohtml/{experiment_name}/"
+        html_files = glob.glob(f"{ofolder}/*.html")
         html_files.sort(reverse=True)
         hfiles = []
         for hfile in html_files:
@@ -77,20 +73,19 @@ def sanityhtml():
 
         # print(html_files)
         title = experiment_name
-        opath = f'./ohtml/{experiment_name}.html'
-        ofile = open(opath, 'w')
-        template = env.get_template('archive.html')
-        output = template.render({'title':title, 
-                                  "hfiles":hfiles,
-                                  'experiment':experiment_name})
+        opath = f"./ohtml/{experiment_name}.html"
+        ofile = open(opath, "w")
+        template = env.get_template("archive.html")
+        output = template.render(
+            {"title": title, "hfiles": hfiles, "experiment": experiment_name}
+        )
         ofile.write(output)
         ofile.close()
 
+    if not os.path.exists("./static/"):
+        static_path = pkg_resources.resource_filename(__name__, f"static")
+        shutil.copytree(static_path, "./static/")
 
-    if not os.path.exists('./static/'):
-        static_path = pkg_resources.resource_filename(__name__,
-                                                        f'static')
-        shutil.copytree(static_path, './static/')
 
 if __name__ == "__main__":
     # args = parser.parse_args()
